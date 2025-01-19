6448e37ac27aa8228cf2ef9199e1505d8701cb64
ceusahin <hakonsahin@gmail.com>
2025-01-19 23:03:05 +0300

    homework done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,103 @@
 package org.example;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
 public class Main {
+    private static final Log log = LogFactory.getLog(Main.class);
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));
+        System.out.println(isPalindrome(707));
+        System.out.println(isPalindrome(11212));
+
+        System.out.println(isPerfectNumber(6));
+        System.out.println(isPerfectNumber(28));
+        System.out.println(isPerfectNumber(496));
+        System.out.println(isPerfectNumber(12));
+        System.out.println(isPerfectNumber(-1));
+
+        System.out.println(numberToWords(123));
+        System.out.println(numberToWords(1010));
+        System.out.println(numberToWords(-12));
     }
+
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        char[] digits = String.valueOf(number).toCharArray();
+        String reversed = "";
+        for (int i = digits.length-1; i>=0 ; i--) {
+            reversed += digits[i];
+
+        }
+        return reversed.equalsIgnoreCase(String.valueOf(number));
+    }
+
+    public static boolean isPerfectNumber(int number) {
+
+        if (number < 0) {
+            return false;
+        }
+
+        int total = 0;
+
+
+        for (int i = 1; i <= number/2 ; i++) {
+            if (number % i == 0) {
+                total += i;
+            }
+        }
+
+        return number == total;
+    }
+
+    public static String numberToWords(int number) {
+        if (number < 0) {
+            return "Invalid Value";
+        }
+        char[] digits = String.valueOf(number).toCharArray();
+        String numToText = "";
+
+        for (char digit : digits) {
+            switch (digit) {
+                case '0':
+                    numToText += "Zero ";
+                    break;
+                case '1':
+                    numToText += "One ";
+                    break;
+                case '2':
+                    numToText += "Two ";
+                    break;
+                case '3':
+                    numToText += "Three ";
+                    break;
+                case '4':
+                    numToText += "Four ";
+                    break;
+                case '5':
+                    numToText += "Five ";
+                    break;
+                case '6':
+                    numToText += "Six ";
+                    break;
+                case '7':
+                    numToText += "Seven ";
+                    break;
+                case '8':
+                    numToText += "Eight ";
+                    break;
+                case '9':
+                    numToText += "Nine ";
+                    break;
+            }
+
+
+        }
+        return numToText.trim();
+
+    }
+
 }
